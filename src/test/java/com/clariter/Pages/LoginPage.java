f5b34a1f2a94e3c467c41d6fd8e712ae70b2b998
Samraaliyeva <samra.aliyeva@claritergroup.com>
2023-05-22 17:19:16 +0200

    Initial commit

---
+++ src/test/java/com/clariter/Pages/LoginPage.java
@@ -0,0 +1,2 @@
+package com.clariter.Pages;public class LoginPage {
+}
---
+++ src/test/java/com/clariter/Step_Definitions/LoginSteps.java
@@ -0,0 +1,25 @@
+package Step_Definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+
+    @Given("the user is on the login page")
+    public void the_user_is_on_the_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user enters his credentials")
+    public void the_user_enters_his_credentials() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the user should be able to login")
+    public void the_user_should_be_able_to_login() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
